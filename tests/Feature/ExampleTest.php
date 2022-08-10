7ff0faf0672fd6f6a9c595f1da29bb50e5d1afb2
João Pedro Garcia <jotapegue09@gmail.com>
2022-08-10 09:18:56 -0300

    adding exemple test in feature directory

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jotapegue\GenerateScaffold\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
